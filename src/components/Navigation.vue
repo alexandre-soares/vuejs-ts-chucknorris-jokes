f46208058eb2ebaf117fe8f45d18098593ec02db
Alexandre Soares <alexandresoares@Alexandres-MacBook-Pro.local>
2021-08-17 12:18:13 +0300

    adding navigation

---
+++ src/components/Navigation.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/search">Search</router-link>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Navigation",
+});
+</script>
+
+<style lang="scss">
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
